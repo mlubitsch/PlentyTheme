7a335dd9b6314bce20bed2471d64b01bd19e9c71
Marc Lubitsch <marc.lubitsch@bookrix.com>
2019-04-18 10:32:45 +0200

    test

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -3,11 +3,21 @@
 namespace PlentyTheme\Providers;
  
  
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
  
 class ThemeServiceProvider extends ServiceProvider{
      
     public function register(){        
     }
     
+	public function boot(Twig $twig, Dispatcher $eventDispatcher){
+        $eventDispatcher->listen('IO.init.templates', function(Partial $partial){
+           $partial->set('header', 'PlentyTheme::content.ThemeHeader');
+        }, 0);
+        return false;
+    }
+
 }
