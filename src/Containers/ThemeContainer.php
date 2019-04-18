4e7fdff4973a3ba2996c850f31de456453300e48
Marc Lubitsch <marc.lubitsch@bookrix.com>
2019-04-18 10:03:55 +0200

    test

--- src/Containers/ThemeContainer.php
+++ src/Containers/ThemeContainer.php
@@ -6,6 +6,6 @@ use Plenty\Plugin\Templates\Twig;
  
 class ThemeContainer{
     public function call(Twig $twig):string{
-        return $twig->render('PlentyTheme::content.Theme');
+        return $twig->render('Theme::content.Theme');
     }
 }
